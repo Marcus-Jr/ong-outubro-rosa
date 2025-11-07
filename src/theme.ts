7178bf29818c4209da3d5f984565cb40217e1cd2
ForgeSolucoesEstagio <juniorfranca632@gmail.com>
2025-11-06 22:44:24 -0300

    style: criando um tema para o projeto

---
+++ src/theme.ts
@@ -0,0 +1,35 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+declare module '@mui/material/styles' {
+  interface Palette {
+    tertiary: Palette['primary'];
+  }
+  interface PaletteOptions {
+    tertiary?: PaletteOptions['primary'];
+  }
+  interface PaletteColor {
+    main: string;
+  }
+}
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F6F1EB',
+    },
+    secondary: {
+      main: '#D9326F',  
+    },
+    tertiary: {
+      main: '#730217'
+    }
+  },
+  typography: {
+    fontFamily: "Franklin Gothic"
+  }
+});
+
+theme.palette.tertiary.contrastText = theme.palette.getContrastText(theme.palette.tertiary.main);
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
